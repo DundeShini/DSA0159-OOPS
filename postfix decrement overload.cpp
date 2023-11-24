060a36475d57a2fb21593ec84c8b80639dbde840
DundeShini <112486766+DundeShini@users.noreply.github.com>
2023-11-24 08:51:35 +0530

    Add files via upload

---
+++ postfix decrement overload.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+class Integer {
+private:
+	int i;
+public:
+	Integer(int i = 0)
+	{
+		this->i = i;
+	}
+	Integer operator--(int)
+	{
+		Integer temp = *this;
+		--i;
+		return temp;
+	}
+	void display()
+	{
+		cout << "i = " << i << endl;
+	}
+};
+int main()
+{
+	Integer i1(3);
+	cout << "Before decrement: ";
+	i1.display();
+	Integer i2 = i1--;
+	cout << "After post decrement: " << endl;
+	cout << "i1: ";
+	i1.display();
+	cout << "i2: "; 
+	i2.display();
+}
+
---
+++ prefix decrement.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class Integer {
+private:
+	int i;
+public:
+	Integer(int i = 0)
+	{
+		this->i = i;
+	}
+	Integer& operator--()
+	{
+		--i;
+		return *this;
+	}
+	void display()
+	{
+		cout << "i = " << i << endl;
+	}
+};
+int main()
+{
+	Integer i1(3);
+	cout << "Before decrement: ";
+	i1.display();
+	Integer i2 = --i1;
+	cout << "After pre decrement: " << endl;
+	cout << "i1: ";
+	i1.display();
+	cout << "i2: "; 
+	i2.display();
+}
+
